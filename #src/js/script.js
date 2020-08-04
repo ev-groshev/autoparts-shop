ac77915d6b56e00f73b47df29d3d3089600fd3ff
Evgeny Groshev <webdev.yeisk@yandex.ru>
2020-08-04 20:24:45 +0300

    banner-section

--- #src/js/script.js
+++ #src/js/script.js
@@ -4,4 +4,9 @@ $(function () {
 		$(this).addClass('active');
 	});
 
+	$('.banner-section__slider').slick({
+		prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="images/arrows-left.svg" alt=""></button>',
+		nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="images/arrows-right.svg" alt=""></button>',
+		dots: true,
+	});
 });
